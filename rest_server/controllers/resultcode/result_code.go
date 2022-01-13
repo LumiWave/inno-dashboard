dfbe165568995711fcad05fdc5a80eacde3be8dc
beomjpark <beomjpark@github.com>
2022-01-13 15:08:22 +0900

    :hammer: Fix result error code

--- rest_server/controllers/resultcode/result_code.go
+++ rest_server/controllers/resultcode/result_code.go
@@ -5,17 +5,17 @@ const (
 
 	Result_Require_PageInfo = 10001 // 유효한 페이지 정보 필요
 
-	Result_Upbit_EmptyCoinSymbol = 12001 // 코인 심볼이 비어있다.
-	Result_Upbit_EmptyUnit       = 12002 // Unit 이 비어있다.
-	Result_Upbit_InvalidUnit     = 12003 // Unit 이 유효하지 않다.
-	Result_Upbit_EmptyCount      = 12004 // Count가 비어있다.
-	Result_Upbit_EmptyTo         = 12005 // To가  비어있다.
+	Result_Upbit_EmptyCoinSymbol = 12001 // 코인 심볼이 비어있음
+	Result_Upbit_EmptyUnit       = 12002 // Unit 이 비어있음
+	Result_Upbit_InvalidUnit     = 12003 // Unit 이 유효하지 않음
+	Result_Upbit_EmptyCount      = 12004 // Count가 비어있음
+	Result_Upbit_EmptyTo         = 12005 // To가  비어있음
 
-	Result_Upbit_TickerMarkets = 13001 // 업비트 시세 Ticker 조회 API 에러.
-	Result_Upbit_CandleMinutes = 13002 // 업비트 CandleDays 조회 API 에러.
-	Result_Upbit_CandleDays    = 13003 // 업비트 CandleDays 조회 API 에러.
-	Result_Upbit_CandleWeeks   = 13004 // 업비트 CandleWeeks 조회 API 에러.
-	Result_Upbit_CandleMonths  = 13005 // 업비트 CandleMonths 조회 API 에러.
+	Result_Upbit_TickerMarkets = 13001 // 업비트 시세 Ticker 조회 API 에러
+	Result_Upbit_CandleMinutes = 13002 // 업비트 CandleMinutes 조회 API 에러
+	Result_Upbit_CandleDays    = 13003 // 업비트 CandleDays 조회 API 에러
+	Result_Upbit_CandleWeeks   = 13004 // 업비트 CandleWeeks 조회 API 에러
+	Result_Upbit_CandleMonths  = 13005 // 업비트 CandleMonths 조회 API 에러
 
 	Result_DBError         = 19000 // db 에러
 	Result_Invalid_DBID    = 19001 // 유효하지 못한 database index
