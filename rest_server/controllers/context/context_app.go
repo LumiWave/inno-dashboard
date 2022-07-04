24b19ac64db16f438b0b75e0fc17768ce9386e2a
byeongtaste <jinwshin@onbuff.com>
2022-07-04 14:25:06 +0900

    :zap: scan coins edit

--- rest_server/controllers/context/context_app.go
+++ rest_server/controllers/context/context_app.go
@@ -42,6 +42,7 @@ type CoinInfo struct {
 	CoinName                        string  `json:"coin_name"`
 	CoinSymbol                      string  `json:"coin_symbol,omitempty"`
 	ContractAddress                 string  `json:"contract_address,omitempty"`
+	ExplorePath                     string  `json:"explore_path"`
 	IconUrl                         string  `json:"icon_url,omitempty"`
 	DailyLimitedAcqExchangeQuantity float64 `json:"daily_limited_acq_exchange_quantity"`
 	ExchangeFees                    float64 `json:"exchange_fees"`
