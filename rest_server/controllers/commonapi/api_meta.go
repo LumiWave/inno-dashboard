20023ba16afc6d24294b3eee15790eb5be2769d6
byeongtaste <jinwshin@onbuff.com>
2022-08-18 16:28:13 +0900

    :zap: polygon disable

--- rest_server/controllers/commonapi/api_meta.go
+++ rest_server/controllers/commonapi/api_meta.go
@@ -13,10 +13,21 @@ func GetMeta(c echo.Context) error {
 	resp := new(base.BaseResponse)
 	resp.Success()
 
+	tmpCoinList := context.CoinList{}
+
+	for _, coin := range model.GetDB().Coins.Coins {
+		if coin.CoinSymbol == "MATIC" {
+			continue
+		}
+
+		tmpCoinList.Coins = append(tmpCoinList.Coins, coin)
+	}
+
 	swapList := context.Meta{
-		PointList:    model.GetDB().ScanPoints,
-		AppPoints:    model.GetDB().AppPoints,
-		CoinList:     model.GetDB().Coins,
+		PointList: model.GetDB().ScanPoints,
+		AppPoints: model.GetDB().AppPoints,
+		//CoinList:     model.GetDB().Coins,
+		CoinList:     tmpCoinList,
 		Swapable:     model.GetDB().SwapAble,
 		BaseCoinList: model.GetDB().BaseCoins,
 	}
